7f3270d5ae987dca179f0c9ad6aafe2d2882d6b1
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2023-01-30 09:34:35 -0700

    TDD addition

--- blockchain.js
+++ blockchain.js
@@ -15,16 +15,24 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      return;
+    }
+    if (!Blockchain.isValidChain(chain)) {
+      return;
+    }
+    this.chain = chain;
+  }
+
   static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
       return false;
 
     for (let i = 1; i < chain.length; i++) {
-      const block = chain[i];
+      const { timestamp, lastHash, hash, data } = chain[i];
 
       const actualLastHash = chain[i - 1].hash;
-
-      const { timestamp, lastHash, hash, data } = block;
 
       if (lastHash !== actualLastHash) return false;
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,12 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-  let blockchain;
+  let blockchain, newChain, originalChain;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    newChain = new Blockchain();
+    originalChain = blockchain.chain;
   });
 
   it("contains a `chain` Array instance", () => {
@@ -58,6 +60,40 @@ describe("Blockchain", () => {
       describe("and the chain does not contain any invalid blocks", () => {
         it("returns true", () => {
           expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
+  });
+
+  describe("replaceChain", () => {
+    describe("when the chain is not longer", () => {
+      it("does not replace the chain", () => {
+        newChain.chain[0] = { new: "chain" };
+
+        blockchain.replaceChain(newChain.chain);
+
+        expect(blockchain.chain).toEqual(originalChain);
+      });
+    });
+    describe("when the new chain is longer", () => {
+      beforeEach(() => {
+        newChain.addBlock({ data: "Bears" });
+        newChain.addBlock({ data: "Beats" });
+        newChain.addBlock({ data: "Battlestar Galactica" });
+      });
+      describe("when the chain is invalid", () => {
+        it("does not replace the chain", () => {
+          newChain.chain[2].hash = "fake-hash";
+
+          blockchain.replaceChain(newChain.chain);
+
+          expect(blockchain.chain).toEqual(originalChain);
+        });
+      });
+      describe("when the chain is valid", () => {
+        it("replaces the chain", () => {
+          blockchain.replaceChain(newChain.chain);
+          expect(blockchain.chain).toEqual(newChain.chain);
         });
       });
     });
