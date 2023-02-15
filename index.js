97f5a0339cb29d19d3de51bf2c034cb57da4f63f
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2023-02-14 20:18:54 -0700

    syncing network nodes

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const pubsub = new PubSub({ blockchain });
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
-
 app.use(bodyParser.json());
 
 app.get("/api/blocks", (req, res) => {
@@ -53,5 +51,7 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
 
-  syncChains();
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
