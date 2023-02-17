1552c1d29250e5c9d973bd154d88fbbccae0d4be
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2023-02-17 16:12:21 -0700

    verifying sig functionality

--- pubSubCredentials.js
+++ pubSubCredentials.js
@@ -1,3 +1,6 @@
+//test credentials for development
+//will hide before deployment
+
 const credentials = {
     publishKey: "pub-c-de31e1dc-6390-401f-9a8e-b17c47235851",
     subscribeKey: "sub-c-3357ff9b-fe45-43c3-b061-d6e58630620a",
