083a1c986a174507b721c8f51c1d4cda9d07b8ba
AReynaldo <alexandrareynaldo1@gmail.com>
2023-03-26 07:14:21 -0700

    new

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("./index");
+const Transaction = require('./transaction')
 const { verifySignature } = require("../util");
 
 describe("Wallet", () => {
@@ -40,4 +41,34 @@ describe("Wallet", () => {
       ).toBe(false);
     });
   });
+
+  describe('createTransaction()', () => {
+    describe('and the amount exceeds the balance', () => {
+      it('throws an error', () => {
+        expect(() => wallet.createTransaction({ amount: 999999, recipient: 'foo' }))
+        .toThrow('Amount exceeds balance')
+      })
+    })
+
+    describe('and the amount is valid', () => {
+      let transaction, amount, recipient
+      
+      beforeEach(() => {
+        amount = 50
+        recipient = 'foo'
+        transaction = wallet.createTransaction({ amount, recipient })
+      })
+      it('creates an instance of `Transaction`', () => {
+        expect(transaction instanceof Transaction).toBe(true)
+      })
+
+      it('matches the transaction input with the wallet', () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey)
+      })
+
+      it('outputs the amount to the recipient', () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount)
+      })
+    })
+  })
 });
